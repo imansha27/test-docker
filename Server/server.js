f836ba1f0b4debbe9a0a00fc605114ad73bec8bf
Pallimulla K Imansha <89239162+imansha27@users.noreply.github.com>
2023-12-05 12:09:13 +0530

    Created server

---
+++ Server/server.js
@@ -0,0 +1,12 @@
+// backend/app.js
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from the backend!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
